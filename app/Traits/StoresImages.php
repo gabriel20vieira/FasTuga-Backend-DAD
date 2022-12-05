87733df044024aec41435fd08168c259f51655ee
Gabriel Vieira <gv1999mpc@hotmail.com>
2022-12-05 17:06:02 +0000

    fix: update image base64 only

--- app/Traits/StoresImages.php
+++ app/Traits/StoresImages.php
@@ -19,13 +19,14 @@ trait StoresImages
         }
         $folder = "public$folder";
 
-        if ($request->hasFile($string)) {
-            $stored = Storage::putFile(
-                $folder,
-                $request->file($string)
-            );
-            return preg_replace("/^public/i", "/storage", $stored);
-        } else if (preg_match("/^data:image\/\w.*;base64/i", $request->input($string))) {
+        // if ($request->hasFile($string)) {
+        //     $stored = Storage::putFile(
+        //         $folder,
+        //         $request->file($string)
+        //     );
+        //     return preg_replace("/^public/i", "/storage", $stored);
+        // } else
+        if (preg_match("/^data:image\/\w.*;base64/i", $request->input($string))) {
 
             preg_match("/data:image\/(.*?);/", $request->input($string), $extension);
             $image = preg_replace('/data:image\/(.*?);base64,/', '', $request->input($string));
